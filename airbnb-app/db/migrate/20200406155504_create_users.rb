205657105b1e6db747c3ce218959c4e203285f9a
bshin95 <shinbryann@gmail.com>
2020-04-06 12:02:02 -0400

    created tables, migrated db

---
+++ airbnb-app/db/migrate/20200406155504_create_users.rb
@@ -0,0 +1,16 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :email
+      t.string :address
+      t.string :city
+      t.string :state
+      t.string :country
+      t.string :zipcode
+      t.string :password_digest
+
+      t.timestamps
+    end
+  end
+end
